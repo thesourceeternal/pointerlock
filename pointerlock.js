dd97dbb905c159b9ef49d82a9c118438a23d8dd7
knod <knodding@gmail.com>
2014-07-13 11:04:01 -0400

    Pointerlock working, attempting getting esc to toggle state

--- pointerlock.js
+++ pointerlock.js
@@ -13,20 +13,49 @@ window.onload = function () {
 	} else {  // This is the bulk of the action
 
 		var lockElement = document.body;
-		console.log(lockElement);
 
 		// Give a name to the automatic pointerlock functions
 		lockElement.requestPointerLock = lockElement.requestPointerLock ||
 			     lockElement.mozRequestPointerLock ||
 			     lockElement.webkitRequestPointerLock;
 
-		// Ask the browser to lock the pointer
-		lockElement.requestPointerLock();
+		// // Ask the browser to lock the pointer
+		// lockElement.requestPointerLock();
 
 		// Hook pointer lock state change events
 		document.addEventListener('pointerlockchange', testPointerLock, false);
 		document.addEventListener('mozpointerlockchange', testPointerLock, false);
 		document.addEventListener('webkitpointerlockchange', testPointerLock, false);
+
+		document.addEventListener( 'pointerlockerror', pointerLockError, false );
+		document.addEventListener( 'mozpointerlockerror', pointerLockError, false );
+		document.addEventListener( 'webkitpointerlockerror', pointerLockError, false );
+
+		var oldKeyCode = null;
+		var keyHeldDown = false;
+
+		document.addEventListener('keydown', function (event) {
+
+			var keyCode = ('which' in event) ? event.which : event.keyCode;
+
+			// Make sure keydown event is only fired once
+			if (!keyHeldDown) {
+
+				// If esc is pressed toggle pointerlock ()
+				if (keyCode === 27) {
+					console.log(keyCode);
+
+					changePointerLock();
+				}
+
+				keyHeldDown = true;
+			}
+
+		});  // end keydown event listener
+
+		document.addEventListener('keyup', function (event) {
+			keyHeldDown = false;
+		});
 
 		testPointerLock();
 		
@@ -49,10 +78,25 @@ window.onload = function () {
 
 			console.log("Pointer is locked");
 
-			// // Get mousemoves in pointerlock way
-			// document.addEventListener("mousemove", this.moveCallback, false);
 		} else {  // Pointer is unlocked
 			console.log("Pointer is FREE!");
+		}
+	}
+
+	function changePointerLock () {
+		// Check for locked pointer
+		if (document.pointerLockElement === lockElement ||
+			document.mozPointerLockElement === lockElement ||
+			document.webkitPointerLockElement === lockElement) {  // Pointer is locked
+
+			lockElement.exitPointerLock();
+
+			// // Get mousemoves in pointerlock way
+			// document.addEventListener("mousemove", this.moveCallback, false);
+
+		} else {  // Pointer is unlocked
+
+			lockElement.requestPointerLock();
 			
 			// // Disable the mousemove listener
 			// document.removeEventListener("mousemove", this.moveCallback, false);
@@ -60,9 +104,10 @@ window.onload = function () {
 			// // I assume this means "do stuff"
 			// this.unlockHook(this.lockElement);
 		}
+
 	}
 
 	function pointerLockError () {
-
+		console.log("Pointer Lock Error");
 	}
 }
