bffc2ff7dfb2e44ab42cc34fa3f9e93a2cdaec99
knod <knodding@gmail.com>
2014-07-13 19:45:27 -0400

    First try at native js promises

--- unittests.js
+++ unittests.js
@@ -9,29 +9,84 @@ window.addEventListener('load', function () {
 	   Call all tests
 	   ==================================== */
 	unitTests = function () {
+		// Need to put tests in a queue
 
 		/* ===================================
 		   Pointer Lock (tests not yet tested)
 		   ==================================== */
+		testToggleLock();
+
+
+	};  // end unitTests
+
+	/* ===================================
+	   Test Functions
+	   ==================================== */
+	testToggleLock = function () {
 		console.log("*** In pointerLock testToggleLock() ***");
 
 		if (pointerLock.lockElement) {
 
-			pointerLock.toggleLock();
-			var test1 = testToggleLock();
+			var test1, test2;
 
-			pointerLock.toggleLock();
-			var test2 = testToggleLock();	
+			// http://www.html5rocks.com/en/tutorials/es6/promises/
+			var promiseTest1 = new Promise(function(resolve, reject) {
 
-			if (test1 !== test2) {
+				pointerLock.toggleLock();
+				test1 = toggleResult();
 
-				console.log("Test passed, pointer lock toggled");
+				if (true) {
+					resolve("Stuff worked!");
+				} else {
+					reject(Error("It broke"));
+				}
 
-			} else {
+				return delay(500);
+			});
 
-				console.log("Test failed, pointer lock not toggled");
+			var promiseTest2 = new Promise(function(resolve, reject) {
 
-			}  // end if test1 !== test2
+				pointerLock.toggleLock();
+				test2 = toggleResult();
+
+				if (true) {
+					resolve("Stuff worked!");
+				} else {
+					reject(Error("It broke"));
+				}
+
+				return delay(500);
+			});
+
+			promiseTest1.then(promiseTest2).then(function () {
+
+				if (test1 !== test2) {
+
+					console.log("Test passed, pointer lock toggled");
+
+				} else {
+
+					console.log("Test failed, pointer lock not toggled");
+
+				}  // end if test1 !== test2
+
+			})
+
+			// pointerLock.toggleLock();
+			// var test1 = toggleResult();
+
+			// pointerLock.toggleLock();
+			// var test2 = toggleResult();	
+
+			// if (test1 !== test2) {
+
+			// 	console.log("Test passed, pointer lock toggled");
+
+			// } else {
+
+			// 	console.log("Test failed, pointer lock not toggled");
+
+			// }  // end if test1 !== test2
 
 
 		} else {
@@ -40,26 +95,22 @@ window.addEventListener('load', function () {
 
 		}  // end pointerLock testToggleLock();
 
-	};  // end unitTests
+		var toggleResult = function () {
+			// Check for locked pointer
+			if (document.pointerLockElement === pointerLock.lockElement ||
+				document.mozPointerLockElement === pointerLock.lockElement ||
+				document.webkitPointerLockElement === pointerLock.lockElement) {  // Pointer is locked
 
-	/* ===================================
-	   Test Functions
-	   ==================================== */
-	testToggleLock = function () {
-		// Check for locked pointer
-		if (document.pointerLockElement === pointerLock.lockElement ||
-			document.mozPointerLockElement === pointerLock.lockElement ||
-			document.webkitPointerLockElement === pointerLock.lockElement) {  // Pointer is locked
+				console.log("Pointer is locked");
+				return true;
 
-			console.log("Pointer is locked");
-			return true;
+			} else {  // Pointer is unlocked
 
-		} else {  // Pointer is unlocked
+				console.log("Pointer is FREE!");
+				return false;
 
-			console.log("Pointer is FREE!");
-			return false;
-
-		}
+			}
+		};  // end toggleResult
 	};  // end testToggleLock()
 
 });
