112f1fb3d5e11a5da3ebc19728630f100a896233
knod <knodding@gmail.com>
2014-07-13 15:51:21 -0400

    Cleaning stuff up, not fit for general consumption though
    It is alright to look at as far as getting an idea of how to
    go about implementing this kind of behavior

--- pointerlock.js
+++ pointerlock.js
@@ -3,27 +3,32 @@
 */
 
 window.onload = function () {
+
 	// Does the user's browser support pointerlock
 	var havePointerLock = 'pointerLockElement' in document ||
 	    'mozPointerLockElement' in document ||
 	    'webkitPointerLockElement' in document;
 
-	if (!havePointerLock) {
+	if (!havePointerLock) {  // For browsers that don't support pointerlock
+
 		alert("Sorry, your browser does not support pointer lock.");
+
 	} else {  // This is the bulk of the action
 
 		var lockElement = document.body;
-		var childElement = document.getElementById("lockDisplay");
 
 		// Give a name to the automatic pointerlock functions
+		// Ask the browser to lock the pointer
 		lockElement.requestPointerLock = lockElement.requestPointerLock ||
 			     lockElement.mozRequestPointerLock ||
 			     lockElement.webkitRequestPointerLock;
 
-		// // Ask the browser to lock the pointer
-		// lockElement.requestPointerLock();
+		// Ask the browser to release/unlock the pointer
+		document.exitPointerLock = document.exitPointerLock ||
+				   document.mozExitPointerLock ||
+				   document.webkitExitPointerLock;
 
-		// Hook pointer lock state change events
+		// Hook pointer lock state change events (automatically assigned strings)
 		document.addEventListener('pointerlockchange', testPointerLock, false);
 		document.addEventListener('mozpointerlockchange', testPointerLock, false);
 		document.addEventListener('webkitpointerlockchange', testPointerLock, false);
@@ -32,41 +37,22 @@ window.onload = function () {
 		document.addEventListener( 'mozpointerlockerror', pointerLockError, false );
 		document.addEventListener( 'webkitpointerlockerror', pointerLockError, false );
 
-		var oldKeyCode = null;
-		var keyHeldDown = false;
+		// Should the esc key lock the pointer bool
+		var escShouldLockPointer = true;
 
-		document.body.addEventListener('keydown', function (event) {
+		document.addEventListener('keyup', function (event) {
 
 			var keyCode = ('which' in event) ? event.which : event.keyCode;
 
-			// Make sure keydown event is only fired once
-			if (!keyHeldDown) {
+			if (keyCode === 27) {
 
-				// If esc is pressed toggle pointerlock ()
-				if (keyCode === 27) {
-					console.log(keyCode);
+				escShouldLockPointer = toggleLock(escShouldLockPointer);
 
-					toggleEsc();
-					event.stopPropagation();
-					event.preventDefault();
-				}
-
-				keyHeldDown = true;
 			}
 
-		});  // end keydown event listener
-
-		document.body.addEventListener('keyup', function (event) {
-			keyHeldDown = false;
-		});
+		}, false);  // end keydown event listener
 
 		testPointerLock();
-		
-		// // Ask the browser to release the pointer
-		// document.exitPointerLock = document.exitPointerLock ||
-		// 			   document.mozExitPointerLock ||
-		// 			   document.webkitExitPointerLock;
-		// document.exitPointerLock();
 		
 	}
 
@@ -74,6 +60,7 @@ window.onload = function () {
 	   Functions
 	   ==================================== */
 	function testPointerLock () {
+		console.log("*** In testPointerLock() ***");
 		// Check for locked pointer
 		if (document.pointerLockElement === lockElement ||
 			document.mozPointerLockElement === lockElement ||
@@ -82,59 +69,40 @@ window.onload = function () {
 			console.log("Pointer is locked");
 
 		} else {  // Pointer is unlocked
+
 			console.log("Pointer is FREE!");
+
 		}
-	}
+	}  // end testPointerLock()
 
+	// Should changing the DOM be here? Probably not, though something should
 	function changePointerLock () {
-		// // Check for locked pointer
-		// if (document.pointerLockElement === lockElement ||
-		// 	document.mozPointerLockElement === lockElement ||
-		// 	document.webkitPointerLockElement === lockElement) {  // Pointer is locked
+	}  // end changePointerLock
 
-		// 	lockElement.exitPointerLock();
+	function toggleLock (escShouldLockPointer) {
 
-		// 	// // Get mousemoves in pointerlock way
-		// 	// document.addEventListener("mousemove", this.moveCallback, false);
-
-		// } else {  // Pointer is unlocked
-
-		// 	lockElement.requestPointerLock();
-			
-		// 	// // Disable the mousemove listener
-		// 	// document.removeEventListener("mousemove", this.moveCallback, false);
-			
-		// 	// // I assume this means "do stuff"
-		// 	// this.unlockHook(this.lockElement);
-		// }
-
-	}
-
-	function toggleEsc () {
-	// Check for locked pointer
-		if (document.pointerLockElement === lockElement ||
-			document.mozPointerLockElement === lockElement ||
-			document.webkitPointerLockElement === lockElement) {  // Pointer is locked
-
-			lockElement.exitPointerLock();
-
-			// // Get mousemoves in pointerlock way
-			// document.addEventListener("mousemove", this.moveCallback, false);
-
-		} else {  // Pointer is unlocked
+		if (escShouldLockPointer) {
 
 			lockElement.requestPointerLock();
-			
-			// // Disable the mousemove listener
-			// document.removeEventListener("mousemove", this.moveCallback, false);
-			
-			// // I assume this means "do stuff"
-			// this.unlockHook(this.lockElement);
-		}
+			escShouldLockPointer = false;
 
-	}
+			return escShouldLockPointer;
+
+		} else {
+			document.exitPointerLock();
+			escShouldLockPointer = true;
+
+			return escShouldLockPointer;
+
+		}
+	}  // end toggleLock()
 
 	function pointerLockError () {
+
 		console.log("Pointer Lock Error");
-	}
-}
+		// TODO: Check out https://dvcs.w3.org/hg/pointerlock/raw-file/default/index.html#dfn-target
+		// for better error messages.
+
+	}  // end pointerLockError()
+
+}  // end window on load
